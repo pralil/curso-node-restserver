b962fe533372d4b6fa2cb2a8decdac17d874afeb
Pablo Ralil <pablo.ralil@gmail.com>
2022-07-13 14:49:01 -0400

    Primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,57 @@
+const { response } = require('express');
+
+
+const usuariosGet = (req, res = response ) => {
+
+    const query = req.query;
+
+    console.log(query);
+
+    res.json({
+        msg: 'get API - controlador',
+        query    
+    })
+}
+
+const usuariosPut = (req, res = response ) => {
+
+    const id = req.params.id;
+
+    res.json({
+        msg: 'put API - controlador',    
+        id
+    })
+}
+
+const usuariosPost = (req, res = response ) => {
+
+    const { nombre, edad } = req.body;
+
+
+    res.json({
+        msg: 'post API - controlador',
+        nombre,
+        edad    
+    })
+}
+
+const usuariosDelete = (req, res = response ) => {
+    res.json({
+        msg: 'delete API - controlador'    
+    })
+}
+
+const usuariosPatch = (req, res = response ) => {
+    res.json({
+        msg: 'patch API - controlador'    
+    })
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch,
+}
